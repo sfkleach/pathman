b23ccf502516c973acd6cd1b708188d8de49f8ec
Stephen Leach <sfkleach@gmail.com>
2025-12-23 21:09:37 +0000

    WIP - go fmt

--- cmd/pathman/main.go
+++ cmd/pathman/main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-    "fmt"
-    "os"
+	"fmt"
+	"os"
 
-    "github.com/sfkleach/pathman/pkg/commands"
+	"github.com/sfkleach/pathman/pkg/commands"
 )
 
 func main() {
-    rootCmd := commands.NewRootCmd()
-    if err := rootCmd.Execute(); err != nil {
-        fmt.Fprintf(os.Stderr, "Error: %v\n", err)
-        os.Exit(1)
-    }
+	rootCmd := commands.NewRootCmd()
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
 }
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,85 +1,85 @@
 package config
 
 import (
-    "encoding/json"
-    "os"
-    "path/filepath"
+	"encoding/json"
+	"os"
+	"path/filepath"
 )
 
 // Config represents the pathman configuration.
 type Config struct {
-    ManagedFolder string `json:"managed_folder"`
+	ManagedFolder string `json:"managed_folder"`
 }
 
 // GetConfigPath returns the path to the configuration file.
 // It uses $XDG_CONFIG_HOME/pathman/config.json if set,
 // otherwise falls back to $HOME/.config/pathman/config.json.
 func GetConfigPath() (string, error) {
-    var configDir string
+	var configDir string
 
-    xdgConfigHome := os.Getenv("XDG_CONFIG_HOME")
-    if xdgConfigHome != "" {
-        configDir = filepath.Join(xdgConfigHome, "pathman")
-    } else {
-        homeDir, err := os.UserHomeDir()
-        if err != nil {
-            return "", err
-        }
-        configDir = filepath.Join(homeDir, ".config", "pathman")
-    }
+	xdgConfigHome := os.Getenv("XDG_CONFIG_HOME")
+	if xdgConfigHome != "" {
+		configDir = filepath.Join(xdgConfigHome, "pathman")
+	} else {
+		homeDir, err := os.UserHomeDir()
+		if err != nil {
+			return "", err
+		}
+		configDir = filepath.Join(homeDir, ".config", "pathman")
+	}
 
-    return filepath.Join(configDir, "config.json"), nil
+	return filepath.Join(configDir, "config.json"), nil
 }
 
 // Load reads the configuration from disk.
 // Returns nil if the config file doesn't exist.
 func Load() (*Config, error) {
-    configPath, err := GetConfigPath()
-    if err != nil {
-        return nil, err
-    }
+	configPath, err := GetConfigPath()
+	if err != nil {
+		return nil, err
+	}
 
-    data, err := os.ReadFile(configPath)
-    if err != nil {
-        if os.IsNotExist(err) {
-            return nil, nil
-        }
-        return nil, err
-    }
+	data, err := os.ReadFile(configPath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil, nil
+		}
+		return nil, err
+	}
 
-    var cfg Config
-    if err := json.Unmarshal(data, &cfg); err != nil {
-        return nil, err
-    }
+	var cfg Config
+	if err := json.Unmarshal(data, &cfg); err != nil {
+		return nil, err
+	}
 
-    return &cfg, nil
+	return &cfg, nil
 }
 
 // Save writes the configuration to disk.
 func (c *Config) Save() error {
-    configPath, err := GetConfigPath()
-    if err != nil {
-        return err
-    }
+	configPath, err := GetConfigPath()
+	if err != nil {
+		return err
+	}
 
-    configDir := filepath.Dir(configPath)
-    if err := os.MkdirAll(configDir, 0755); err != nil {
-        return err
-    }
+	configDir := filepath.Dir(configPath)
+	if err := os.MkdirAll(configDir, 0755); err != nil {
+		return err
+	}
 
-    data, err := json.MarshalIndent(c, "", "    ")
-    if err != nil {
-        return err
-    }
+	data, err := json.MarshalIndent(c, "", "    ")
+	if err != nil {
+		return err
+	}
 
-    return os.WriteFile(configPath, data, 0644)
+	return os.WriteFile(configPath, data, 0644)
 }
 
 // GetDefaultManagedFolder returns the default path for the managed folder.
 func GetDefaultManagedFolder() (string, error) {
-    homeDir, err := os.UserHomeDir()
-    if err != nil {
-        return "", err
-    }
-    return filepath.Join(homeDir, ".local", "bin", "pathman-links"), nil
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	return filepath.Join(homeDir, ".local", "bin", "pathman-links"), nil
 }
